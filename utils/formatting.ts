ef13a0413116eaf321fd5494fe529b0810a01d9e
kamarlaylatt <kamarehtha96@gmail.com>
2026-03-16 18:45:23 +0700

    Change currency formatting from IDR to USD

--- utils/formatting.ts
+++ utils/formatting.ts
@@ -1,5 +1,5 @@
 export const formatIDR = (value: number | string) => {
   const num = parseFloat(String(value));
-  const formatted = new Intl.NumberFormat('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(num);
+  const formatted = new Intl.NumberFormat('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(num);
   return `💰 ${formatted}`;
 };
