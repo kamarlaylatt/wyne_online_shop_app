1a5b2fdf4fee6888a629a01683843e407e127057
kamarlaylatt <kamarehtha96@gmail.com>
2026-03-17 20:16:48 +0700

    Update API base URL and add request logging

--- services/http.ts
+++ services/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Constants from 'expo-constants';
 import { authClient } from './authClient';
 
-const baseUrl = Constants.expoConfig?.extra?.apiBaseUrl ?? 'http://157.245.197.26';
+const baseUrl = process.env.EXPO_PUBLIC_API_BASE_URL ?? Constants.expoConfig?.extra?.apiBaseUrl ?? 'http://157.245.197.26';
 
 const http = axios.create({
   baseURL: baseUrl + '/api/admin',
@@ -10,6 +10,7 @@ const http = axios.create({
 });
 
 http.interceptors.request.use(async (config) => {
+  console.log('[http]', config.method?.toUpperCase(), baseUrl + config.url);
   const cookies = await authClient.getCookie();
   if (cookies) {
     config.headers['Cookie'] = cookies;
